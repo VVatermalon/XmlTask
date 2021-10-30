d9db96629a9d5d90e7475560ca7725a9e5e7f3b2
VVatermalon <VVatermelon4ik@gmail.com>
2021-10-30 19:25:35 +0300

    xsd, sml

--- src/main/java/by/skarulskaya/cards/entity/Card.java
+++ src/main/java/by/skarulskaya/cards/entity/Card.java
@@ -3,9 +3,10 @@ package by.skarulskaya.cards.entity;
 
 public abstract class Card {
     id required
-    theme enum
-    type enum
-    country enum
+    theme enum cityscape people, religion, sports,
+            architecture, animals, cars
+    type enum greeting advertising default
+    country enum Belarus russia poland lithuania
     author optional string
     звук есть ци не boolean restriction?
 }
